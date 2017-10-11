c5db4aabb55c68604ca09b90ddb2712402e61367
lokiloko <ianw.tkj2@gmail.com>
2017-10-11 16:26:31 +0700

    commit ajah

--- routes/doExamRoute.js
+++ routes/doExamRoute.js
@@ -4,6 +4,7 @@ const sequelize = require('sequelize');
 const router = express.Router();
 const ExamCtrl = require('../controllers/examCtrl');
 const DoExamCtrl = require('../controllers/doExamCtrl');
+var pdfMaker = require('pdf-maker');
 
 router.post('/', function(req, res) {
   DoExamCtrl.getTest(req, res);
@@ -13,8 +14,27 @@ router.post('/periksa', function(req, res) {
   DoExamCtrl.checkTest(req, res);
 })
 
-router.get('/complete/:name/:score/:date', (req, res) => {
-  res.send(req.params.name + req.params.score + req.params.date);
+router.get('/complete/:name/:score/:date/', (req, res) => {
+  var template = path.join(__dirname, '../views/boom.ejs');
+  var datab = {
+    fullname:req.params.name,
+    score:req.params.score,
+    awardDate:req.params.date,
+    examName:req.params.examName,
+    resultId:req.params.id
+  }
+  var pdfPath = 'pdf/'+req.params.id+'.pdf';
+  var option = {
+      paperSize: {
+        format: 'A4',
+        orientation: 'portrait',
+        border: '1.8cm'
+      }
+  };
+
+  pdfMaker(template, datab, pdfPath);
+  datab.isWebpage = true;
+  res.render('certificate', {datab})
 });
 
 module.exports = router;
